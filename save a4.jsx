c43759e34e3492a033abc4a67173b69dbe182e1b
Hoangphohieu <hoangphohieu1@gmail.com>
2019-11-26 17:47:41 +0800

    heu

--- save a4.jsx
+++ save a4.jsx
@@ -1,70 +1,80 @@
-// tao folder a5
+app.preferences.rulerUnits = Units.PIXELS; // hệ đo pixel
 var folderA4 = Folder("~/Desktop/save a4");
-if (!folderA4.exists) { folderA4.create(); }
+if (folderA4.exists) {
+    alert("please delete folder 'save a4' and try it again ");
+}
+else {
+    folderA4.create();
+    var customerChildren = Folder("~/Desktop/save customer").getFiles("*");
+    for (var i = 0; i < customerChildren.length; i++) {
+        var currentFolder = String(customerChildren[i]).split("/");   // đường dẫn thư mục là  customerChildren[i]
+        currentFolder = currentFolder[currentFolder.length - 1];
+        if (currentFolder.split(".").length === 1) { // chỉ chọn những folder. còn file thì không
+            var ListFileChildren = Folder(customerChildren[i]).getFiles("*");
+            if (ListFileChildren.length < 13) {
+                alert(" co cuon mot mat khong du 13 file");
+            }
+            else if (ListFileChildren.length < 15) {
+                try {
+                    jpgOption = new JPEGSaveOptions();
+                    jpgOption.quality = 12;
 
-// var stCheck = "t%20(1).jpg";
-var stCheck = "bia.jpg,t (1).jpg,t (2).jpgcurrentFile,t (4).jpg,t (5).jpg,t (6).jpg,t (7).jpg,t (8).jpg,t (9).jpg,t (10).jpg,t (11).jpg,t (12).jpg";
-var customerChildren = Folder("~/Desktop/save customer").getFiles("*");
-for (var i = 0; i < customerChildren.length; i++) {
-    var currentFolder = String(customerChildren[i]).split("/");   // đường dẫn thư mục là  customerChildren[i]
-    currentFolder = currentFolder[currentFolder.length - 1];
-    if (currentFolder.split(".").length === 1) { // chỉ chọn những folder. còn file thì không
-        var ListFileChildren = Folder(customerChildren[i]).getFiles("*");
-        if (ListFileChildren.length < 13) {
-            alert(" co thu muc khong du 13 file");
-        }
-        else if (ListFileChildren.length === 13) {
-            // alert(ListFileChildren);
-            // alert(stCheck);
-
-
-            try {
-                jpgOption = new JPEGSaveOptions();
-                jpgOption.quality = 12;
-
-                app.documents.add(2480, 3508, 300, "a4");
-                app.open(File(customerChildren[i] + "/bia.jpg"));
-                app.doAction("duplicate to a4", "calendar");
-                app.doAction("xep to bia a4", "calendar");
-                app.activeDocument.saveAs(Folder("~/Desktop/save a4/" + currentFolder + " (1).jpg"), jpgOption, true, Extension.LOWERCASE);
-                app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-
-
-
-
-                // lam 12 thang
-                app.documents.add(2480, 3508, 300, "a4");
-                for (var i2 = 1; i2 <= 12; i2 = i2 + 2) {
-                    // alert(customerChildren[i]+"/t ("+i2+").jpg");
-                    app.open(File(customerChildren[i] + "/t%20(" + i2 + ").jpg"));
+                    // lam to bia
+                    app.documents.add(2480, 3508, 300, "a4");
+                    app.open(File(customerChildren[i] + "/bia.jpg"));
                     app.doAction("duplicate to a4", "calendar");
                     app.doAction("can giua", "calendar");
                     app.doAction("can le tren", "calendar");
 
-                    app.open(File(customerChildren[i] + "/t%20(" + (i2 + 1) + ").jpg"));
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le duoi", "calendar");
-
-                    app.doAction("hop nhat", "calendar");
-                    app.activeDocument.duplicate(currentFolder, false);
-
-                    app.activeDocument.saveAs(Folder("~/Desktop/save a4/" + currentFolder + " (" + (((i2 + 1) / 2) + 1) + ").jpg"), jpgOption, true, Extension.LOWERCASE);
+                    try {
+                        app.open(File(customerChildren[i] + "/biasau.jpg"));
+                        app.doAction("duplicate to a4", "calendar");
+                        app.doAction("can giua", "calendar");
+                        app.doAction("can le duoi", "calendar");
+                    } catch (error) {
+                        app.open(File(customerChildren[i] + "/bia.jpg"));
+                        app.doAction("duplicate to a4", "calendar");
+                        app.doAction("can giua", "calendar");
+                        app.doAction("can le duoi", "calendar");
+                    }
+                    app.activeDocument.saveAs(Folder("~/Desktop/save a4/" + currentFolder + " (1).jpg"), jpgOption, true, Extension.LOWERCASE);
                     app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-                }
-                app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
 
-            } catch (error) {
+                    // lam 12 thang
+                    app.documents.add(2480, 3508, 300, "a4");
+                    for (var i2 = 1; i2 <= 12; i2 = i2 + 2) {
+                        // alert(customerChildren[i]+"/t ("+i2+").jpg");
+                        app.open(File(customerChildren[i] + "/t%20(" + i2 + ").jpg"));
+                        app.doAction("duplicate to a4", "calendar");
+                        app.doAction("can giua", "calendar");
+                        app.doAction("can le tren", "calendar");
+
+                        app.open(File(customerChildren[i] + "/t%20(" + (i2 + 1) + ").jpg"));
+                        app.doAction("duplicate to a4", "calendar");
+                        app.doAction("can giua", "calendar");
+                        app.doAction("can le duoi", "calendar");
+
+                        app.doAction("hop nhat", "calendar");
+                        app.activeDocument.duplicate(currentFolder, false);
+
+                        app.activeDocument.saveAs(Folder("~/Desktop/save a4/" + currentFolder + " (" + (((i2 + 1) / 2) + 1) + ").jpg"), jpgOption, true, Extension.LOWERCASE);
+                        app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+                    }
+                    app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+
+                } catch (error) {
+                    alert("Dat sai ten, vui long thu lai");
+                }
+
+
 
             }
-
-
-
+            else{}
         }
 
     }
-
 }
+
 // var name2 = String(inputFolder[0]).split("/");
 // var name3 = name2[name2.length - 1]
 
--- save a5.jsx
+++ save a5.jsx
@@ -1,3 +1,6 @@
+app.preferences.rulerUnits = Units.PIXELS; // hệ đo pixel
+
+
 // tao folder a5
 var folderA5 = Folder("~/Desktop/save a5");
 if (!folderA5.exists) { folderA5.create(); }
@@ -7,17 +10,23 @@ var folderCustomer = Folder("~/Desktop/save customer");
 if (!folderCustomer.exists) { folderCustomer.create(); }
 
 
-
-try {
-    var thisDocument = app.activeDocument.name;
-    var nameSave = thisDocument.split(".")[0];
-    var fileName = Folder("~/Desktop/save a5/" + nameSave);
-    jpgOption = new JPEGSaveOptions();
-    jpgOption.quality = 12;
-    app.activeDocument.saveAs(fileName, jpgOption, true, Extension.LOWERCASE);
-    app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-
-} catch (error) {
-    alert("have no thing to do !");
-
+var w=app.activeDocument.width;
+var h=app.activeDocument.height;
+if(w==="2480 px" && h==="1754 px"){
+    try {
+        var thisDocument = app.activeDocument.name;
+        var nameSave = thisDocument.split(".")[0];
+        var fileName = Folder("~/Desktop/save a5/" + nameSave);
+        jpgOption = new JPEGSaveOptions();
+        jpgOption.quality = 12;
+        app.activeDocument.saveAs(fileName, jpgOption, true, Extension.LOWERCASE);
+        app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+    
+    } catch (error) {
+        alert("have no thing to do !");
+    
+    }
+}
+else{
+    alert("crop document ve co 2480px : 1754px");
 }
