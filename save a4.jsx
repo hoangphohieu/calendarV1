7a9e223974adf2bdeca05ad6f46d682f67598a4f
Hoangphohieu <hoangphohieu1@gmail.com>
2020-12-27 11:44:12 +0700

    hieu dz

--- save a4.jsx
+++ save a4.jsx
@@ -31,7 +31,7 @@ else {
                 Folder("~/Desktop/save a4/1 mat").create();
 
                 // lam to bia tren
-                app.documents.add(3720, 5262, 450, "a4");
+                app.documents.add(2480, 3508, 300, "a4");
                 app.open(File(customerChildren[i] + "/bia.jpg"));
                 app.doAction("duplicate to a4", "calendar");
                 app.doAction("can giua", "calendar");
@@ -51,47 +51,6 @@ else {
 
                 app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (1).jpg"), jpgOption, true, Extension.LOWERCASE);
 
-                // lam thang 11 2020
-                try {
-                    app.open(File(customerChildren[i] + "/t11%202020.jpg"));
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le tren", "calendar");
-
-                    app.open(File(customerChildren[i + 1] + "/t11%202020.jpg"));
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le duoi", "calendar");
-
-                } catch (error) {
-                    // loi khi so quyen la le
-                }
-                app.doAction("hop nhat", "calendar");
-                app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (2).jpg"), jpgOption, true, Extension.LOWERCASE);
-
-                // lam thang 12 2020
-                try {
-                    app.open(File(customerChildren[i] + "/t12%202020.jpg"));
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le tren", "calendar");
-
-                    app.open(File(customerChildren[i + 1] + "/t12%202020.jpg"));
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le duoi", "calendar");
-
-                } catch (error) {
-                    // loi khi so quyen la le
-                }
-                app.doAction("hop nhat", "calendar");
-                app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (3).jpg"), jpgOption, true, Extension.LOWERCASE);
-
-
-
-
-
-
                 // lam 12 thang
                 for (var i2 = 1; i2 <= 12; i2++) {
                     app.open(File(customerChildren[i] + "/t%20(" + i2 + ").jpg"));
@@ -110,32 +69,32 @@ else {
 
                     app.doAction("hop nhat", "calendar");
 
-                    app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (" + (i2 + 3) + ").jpg"), jpgOption, true, Extension.LOWERCASE);
+                    app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (" + (i2 + 1) + ").jpg"), jpgOption, true, Extension.LOWERCASE);
 
 
                 }
                 
 
 
-                // lam trang cuoi
-                try {
-                    app.open(File("~/Desktop/to%20cuoi.psd"));
-                    app.activeDocument.artLayers["sku"].textItem.contents = "J" + ngay + thang + (i+1);
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le tren", "calendar");
+                // // lam trang cuoi
+                // try {
+                //     app.open(File("~/Desktop/to%20cuoi.psd"));
+                //     app.activeDocument.artLayers["sku"].textItem.contents = "J" + ngay + thang + (i+1);
+                //     app.doAction("duplicate to a4", "calendar");
+                //     app.doAction("can giua", "calendar");
+                //     app.doAction("can le tren", "calendar");
 
-                    app.open(File("~/Desktop/to%20cuoi.psd"));
-                    app.activeDocument.artLayers["sku"].textItem.contents = "J" + ngay + thang + (i + 2);
-                    app.doAction("duplicate to a4", "calendar");
-                    app.doAction("can giua", "calendar");
-                    app.doAction("can le duoi", "calendar");
+                //     app.open(File("~/Desktop/to%20cuoi.psd"));
+                //     app.activeDocument.artLayers["sku"].textItem.contents = "J" + ngay + thang + (i + 2);
+                //     app.doAction("duplicate to a4", "calendar");
+                //     app.doAction("can giua", "calendar");
+                //     app.doAction("can le duoi", "calendar");
 
-                } catch (error) {
-                    // loi khi so quyen la le
-                }
-                app.doAction("hop nhat", "calendar");
-                app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (16).jpg"), jpgOption, true, Extension.LOWERCASE);
+                // } catch (error) {
+                //     // loi khi so quyen la le
+                // }
+                // app.doAction("hop nhat", "calendar");
+                // app.activeDocument.saveAs(Folder("~/Desktop/save a4/1 mat/" + i + "-" + (i + 1) + " (16).jpg"), jpgOption, true, Extension.LOWERCASE);
 
 
                 app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
@@ -252,8 +211,7 @@ function KiemTraThuMuc(customerChildren) {
                     || name === "s%20(10).jpg"
                     || name === "s%20(11).jpg"
                     || name === "s%20(12).jpg"
-                    || name === "t11%202020.jpg"
-                    || name === "t12%202020.jpg"
+
                 )
 
             ) alert(currentFolder);
